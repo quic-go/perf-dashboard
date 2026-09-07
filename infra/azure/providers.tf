1b2d4fb476cf62f4a2d93f9955b228d36037b9d4
Marten Seemann <martenseemann@gmail.com>
2026-09-07 15:53:49 +0800

    track partially created Azure resources and force-delete VMs

--- infra/azure/providers.tf
+++ infra/azure/providers.tf
@@ -1,5 +1,12 @@
 provider "azurerm" {
   subscription_id = var.azure_subscription_id
 
-  features {}
+  features {
+    # keep partially created resources in state so destroy can remove them
+    persist_id_on_create_before_polling_for_completion = true
+
+    virtual_machine {
+      skip_shutdown_and_force_delete = true
+    }
+  }
 }
